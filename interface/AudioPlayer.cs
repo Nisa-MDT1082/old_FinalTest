de0dd20529b00e72ea135e9f91795f03f472bf04
Nisa-MDT1082 <111278187+Nisa-MDT1082@users.noreply.github.com>
2022-10-01 18:42:12 +0700

    Add files via upload

---
+++ interface/AudioPlayer.cs
@@ -0,0 +1,28 @@
+interface IAudioPlayer{
+    void Play();
+    void Pause();
+    void Stop();
+}
+interface IMusic{
+    void MusicName();
+    void Melody();
+}
+class AudioPlayer: IAudioPlayer, IMusic{
+    public void Play(){
+        Console.WriteLine("Play");
+    }
+
+    public void Pause(){
+        Console.WriteLine("Pause");
+    }
+    public void Stop(){
+        Console.WriteLine("Stop");
+    }
+    public void MusicName(){
+        Console.WriteLine("8 year");
+    }
+
+    public void Melody(){
+        Console.WriteLine("Tuuuuuuu");
+    }
+}
---
+++ interface/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program{
+    static void Main(string[] args){
+        AudioPlayer player = new AudioPlayer();
+
+        player.Play();
+    }
+}
